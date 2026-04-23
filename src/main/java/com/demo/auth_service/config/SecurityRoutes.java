a28182954b8749013f487a478fe52f9f0d0ce0ae
agustin <aggarcia997@gmail.com>
2026-04-23 19:11:31 -0300

    feat(security): add SecurityConfig with JWT and route authorization

--- src/main/java/com/demo/auth_service/config/SecurityRoutes.java
+++ src/main/java/com/demo/auth_service/config/SecurityRoutes.java
@@ -1,16 +1,18 @@
 package com.demo.auth_service.config;
 
+
+
 public class SecurityRoutes {
 
     private SecurityRoutes() {}
 
-    private static final  String[] PUBLIC_ROUTES = {
+    public static final  String[] PUBLIC_ROUTES = {
             "/api/v1/auth/register",
             "/api/v1/auth/login",
             "/api/v1/auth/refresh-token",
     };
 
-    private static final  String[] PRIVATE_ROUTES = {
+    public static final  String[] PRIVATE_ROUTES = {
             "/api/v1/auth/**",
     };
 }
