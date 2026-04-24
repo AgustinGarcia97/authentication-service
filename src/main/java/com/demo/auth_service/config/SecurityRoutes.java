494e6f9f5929dfe4871d9fb6a5101813717e687b
agustin <aggarcia997@gmail.com>
2026-04-24 00:36:40 -0300

    feat(swagger): add OpenAPI documentation with JWT support

--- src/main/java/com/demo/auth_service/config/SecurityRoutes.java
+++ src/main/java/com/demo/auth_service/config/SecurityRoutes.java
@@ -10,6 +10,9 @@ public class SecurityRoutes {
             "/api/v1/auth/register",
             "/api/v1/auth/login",
             "/api/v1/auth/refresh-token",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**"
 
     };
 
